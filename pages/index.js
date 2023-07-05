79da573dc630f875465443925d301fc830f6391e
James Lang <hi@thejameslang.com>
2023-07-05 11:43:09 -0500

    Animate work section

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {
   Image,
   useColorModeValue
 } from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
   return (
@@ -44,6 +45,12 @@ const Page = () => {
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
