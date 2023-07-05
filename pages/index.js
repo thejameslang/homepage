ae6ca86da2e15c9da3f33c3298b836d9f4a4f97c
James Lang <hi@thejameslang.com>
2023-07-05 11:30:34 -0500

    Add profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image } from '@chakra-ui/react'
 const Page = () => {
   return (
     <Container>
@@ -12,6 +12,23 @@ const Page = () => {
             James Lang
           </Heading>
           <p>Digital Artisan ( Artist / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/james.png"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
