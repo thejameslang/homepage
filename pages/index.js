7096d7975a2e541884939bd57397077bad5162b5
James Lang <hi@thejameslang.com>
2023-07-05 13:26:46 -0500

    Create bio section

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ import {
   Heading,
   Image,
   useColorModeValue,
-  Button
+  Button,
+  Link
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -67,6 +69,59 @@ const Page = () => {
             </Button>
           </NextLink>
         </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2005</BioYear>
+          Completed the Music program of the Pennsylvania Governor's School for
+          the Arts
+        </BioSection>
+        <BioSection>
+          <BioYear>2013</BioYear>
+          Completed the Econometrics Track of the undergraduate Economics degree
+          in the College of the Liberal Arts at Penn State
+        </BioSection>
+        <BioSection>
+          <BioYear>2013</BioYear>
+          Worked at Capital One
+        </BioSection>
+        <BioSection>
+          <BioYear>2017</BioYear>
+          Worked at CBRE
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Worked at Indeed
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Cooking, Drawing,{' '}
+          <Link
+            href="https://drive.google.com/file/d/1Iak8OqqY1RBSPAX07R7QJckroVX7UZST/view?usp=sharing"
+            target="_blank"
+          >
+            Playing Piano
+          </Link>
+          , Touge (
+          <Link href="https://youtu.be/WnSDuSxofJs" target="_blank">
+            Kansei Dorifto
+          </Link>
+          ),{' '}
+          <Link
+            href="https://drive.google.com/file/d/1a0nqRfnrEnrXzs6T8HAcFsIIEK9hQIQc/view?usp=sharing"
+            target="_blank"
+          >
+            Wushu
+          </Link>
+        </Paragraph>
       </Section>
     </Container>
   )
