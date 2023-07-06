1db8f1e83567cf5e26567e09eadfd8ff2ef0e1af
James Lang <hi@thejameslang.com>
2023-07-06 12:44:07 -0500

    fix typo

--- components/voxel-pc.js
+++ components/voxel-pc.js
@@ -125,7 +125,7 @@ const VoxelPC = () => {
       ref={refContainer}
       className="voxel-pc"
       m="auto"
-      at={['-20px', '-60px', '-120px']}
+      mt={['-20px', '-60px', '-120px']}
       mb={['-40px', '-140px', '-200px']}
       w={[280, 480, 640]}
       h={[280, 480, 640]}
