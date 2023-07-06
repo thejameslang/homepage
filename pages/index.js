274c44485b39d228d01d8103465fc90b7df40399
James Lang <hi@thejameslang.com>
2023-07-06 09:29:13 -0500

    wrap in animated article

--- pages/index.js
+++ pages/index.js
@@ -9,125 +9,129 @@ import {
   Link
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Dallas!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            James Lang
-          </Heading>
-          <p>Digital Artisan ( Artist / Developer / Designer )</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/james.png"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a full-stack developer based in Dallas!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          As a full-stack developer and freelancer based in Dallas, I enjoy
-          creating digital solutions for intriguing challenges. I have a knack
-          for launching products, from planning and designing to coding and
-          problem-solving. When I&apos;m offline, I love tickling the ivories.
-          I&apos;m currently looking for opportunities in software engineering.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <Button
-            as={NextLink}
-            href="/works"
-            scroll={false}
-            rightIcon={<ChevronRightIcon />}
-            colorScheme="teal"
-          >
-            My portfolio
-          </Button>
-        </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>2005</BioYear>
-          Completed the Music program of the Pennsylvania Governor's School for
-          the Arts
-        </BioSection>
-        <BioSection>
-          <BioYear>2013</BioYear>
-          Completed the Econometrics Track of the undergraduate Economics degree
-          in the College of the Liberal Arts at Penn State
-        </BioSection>
-        <BioSection>
-          <BioYear>2013</BioYear>
-          Worked at Capital One
-        </BioSection>
-        <BioSection>
-          <BioYear>2017</BioYear>
-          Worked at CBRE
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Worked at Indeed
-        </BioSection>
-      </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Cooking, Drawing,{' '}
-          <Link
-            href="https://drive.google.com/file/d/1Iak8OqqY1RBSPAX07R7QJckroVX7UZST/view?usp=sharing"
-            target="_blank"
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              James Lang
+            </Heading>
+            <p>Digital Artisan ( Artist / Developer / Designer )</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
           >
-            Playing Piano
-          </Link>
-          , Touge (
-          <Link href="https://youtu.be/WnSDuSxofJs" target="_blank">
-            K-Kansei Dorifto???
-          </Link>
-          ),{' '}
-          <Link
-            href="https://drive.google.com/file/d/1a0nqRfnrEnrXzs6T8HAcFsIIEK9hQIQc/view?usp=sharing"
-            target="_blank"
-          >
-            Wushu
-          </Link>
-        </Paragraph>
-      </Section>
-    </Container>
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/james.png"
+              alt="Profile Image"
+            />
+          </Box>
+        </Box>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            As a full-stack developer and freelancer based in Dallas, I enjoy
+            creating digital solutions for intriguing challenges. I have a knack
+            for launching products, from planning and designing to coding and
+            problem-solving. When I&apos;m offline, I love tickling the ivories.
+            I&apos;m currently looking for opportunities in software
+            engineering.
+          </Paragraph>
+          <Box align="center" my={4}>
+            <Button
+              as={NextLink}
+              href="/works"
+              scroll={false}
+              rightIcon={<ChevronRightIcon />}
+              colorScheme="teal"
+            >
+              My portfolio
+            </Button>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>2005</BioYear>
+            Completed the Music program of the Pennsylvania Governor's School
+            for the Arts
+          </BioSection>
+          <BioSection>
+            <BioYear>2013</BioYear>
+            Completed the Econometrics Track of the undergraduate Economics
+            degree in the College of the Liberal Arts at Penn State
+          </BioSection>
+          <BioSection>
+            <BioYear>2013</BioYear>
+            Worked at Capital One
+          </BioSection>
+          <BioSection>
+            <BioYear>2017</BioYear>
+            Worked at CBRE
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Worked at Indeed
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            Cooking, Drawing,{' '}
+            <Link
+              href="https://drive.google.com/file/d/1Iak8OqqY1RBSPAX07R7QJckroVX7UZST/view?usp=sharing"
+              target="_blank"
+            >
+              Playing Piano
+            </Link>
+            , Touge (
+            <Link href="https://youtu.be/WnSDuSxofJs" target="_blank">
+              K-Kansei Dorifto???
+            </Link>
+            ),{' '}
+            <Link
+              href="https://drive.google.com/file/d/1a0nqRfnrEnrXzs6T8HAcFsIIEK9hQIQc/view?usp=sharing"
+              target="_blank"
+            >
+              Wushu
+            </Link>
+          </Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,4 +1,5 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
@@ -8,53 +9,57 @@ import thumbAssistek from '../public/images/works/assistek_eyecatch.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="inkdrop"
-            title="Capital One Home Equity"
-            thumbnail={thumbCapitalOne}
-          >
-            Front end for Capital One's home equity offerings: prequalification
-            process, home equity loans and home equity lines of credit
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-
-      <Section delay={0.4}>
-        <Divider my={6} />
-
+    <Layout>
+      <Container>
         <Heading as="h3" fontSize={20} mb={4}>
-          Old works
+          Works
         </Heading>
-      </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.5}>
-          <WorkGridItem
-            id="vouchering"
-            thumbnail={thumbVouchering}
-            title="CBRE Capital Markets Vouchering Platform"
-          >
-            CRM for Capital Markets producers
-          </WorkGridItem>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="inkdrop"
+              title="Capital One Home Equity"
+              thumbnail={thumbCapitalOne}
+            >
+              Front end for Capital One's home equity offerings:
+              prequalification process, home equity loans and home equity lines
+              of credit
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+
+        <Section delay={0.4}>
+          <Divider my={6} />
+
+          <Heading as="h3" fontSize={20} mb={4}>
+            Old works
+          </Heading>
         </Section>
-        <Section delay={0.5}>
-          <WorkGridItem
-            id="assistek"
-            thumbnail={thumbAssistek}
-            title="assisTek eDiary app for clinical trial studies"
-          >
-            Android app for clinical trial studies conducted on Android tablets
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="vouchering"
+              thumbnail={thumbVouchering}
+              title="CBRE Capital Markets Vouchering Platform"
+            >
+              CRM for Capital Markets producers
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="assistek"
+              thumbnail={thumbAssistek}
+              title="assisTek eDiary app for clinical trial studies"
+            >
+              Android app for clinical trial studies conducted on Android
+              tablets
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
